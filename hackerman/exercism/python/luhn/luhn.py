88037aa38e73ca116ffda9b5c0de43d5215e23e0
Igor Cavalcanti <igor.lima@luizalabs.com>
2020-05-05 22:20:26 -0300

    adding luhn exercism solution

--- hackerman/exercism/python/luhn/luhn.py
+++ hackerman/exercism/python/luhn/luhn.py
@@ -1,21 +1,24 @@
 class Luhn:
     def __init__(self, card_num):
-        self.__card_num = card_num
+        self.__card_num = str(card_num).strip()
 
     def valid(self):
         try:
-            if self.__card_num.__len__() > 1:
-                luhn = [int(x) for x in self.__card_num.strip()]
-                for x in range(luhn.__len__() - 2, 0, -2):
-                    print('X: ' + str(luhn[x]))
-                print(luhn)
-                self.__card_num = self.__card_num.strip()
+            card_num = self.__card_num.replace(' ', '')
+            if card_num.__len__() > 1:
+                card_num = [int(x) for x in card_num[::-1]]
+                for x in range(1, card_num.__len__(), 2):
+                    doubled = int(card_num[x]) * 2
+                    if doubled > 9:
+                        card_num[x] = doubled - 9
+                    else:
+                        card_num[x] = doubled
+                if sum(card_num) % 10 == 0:
+                    return True
+                else:
+                    return False
             else:
                 return False
         except Exception as e:
             print(e)
             return False
-
-if __name__ == "__main__":
-    print(Luhn("0591 1234").valid())
-
